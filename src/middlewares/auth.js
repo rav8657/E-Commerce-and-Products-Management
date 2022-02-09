1ad61adc78435a27e4a06aad6b860e73733d18a0
rav8657 <rav8657@gmail.com>
2022-02-09 18:50:34 +0530

    finalCode

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -8,15 +8,23 @@ const userAuth = async(req, res, next) => {
         }
         let splitToken = token.split(' ')
 
-        let decodeToken = jwt.decode(splitToken[1], 'Hercules')
+        let decodeToken = jwt.decode(splitToken[1])
+
         if (!decodeToken) {
             return res.status(403).send({ status: false, message: `Invalid authentication token in request ` })
         }
         if (Date.now() > (decodeToken.exp) * 1000) {
             return res.status(404).send({ status: false, message: `Session Expired, please login again` })
         }
+        const verify = jwt.verify(splitToken[1], 'Hercules')
+        
+        if (!verify) {
+            return res.status(403).send({ status: false, message: `Invalid authentication token in request ` })
+        }
+
         req.userId = decodeToken.userId
         next()
+
     } catch (err) {
         res.status(500).send({ status: false, message: err.message })
     }
--- src/utils/validator.js
+++ src/utils/validator.js
@@ -53,3 +53,5 @@ module.exports = {
     validQuantity,
     isValidStatus
 }
+
+
